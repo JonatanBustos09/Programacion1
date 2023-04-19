1c8cc6670d7a7c6276def327e0de87d6c9d4987b
JonatanBustos09 <131210781+JonatanBustos09@users.noreply.github.com>
2023-04-19 18:42:50 -0300

    Commet1

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Proyecto_1
+{
+    public class Program
+    {
+        static void Main()
+        {
+             Console.WriteLine("Hola te voy a ir pidiendo los valores de los 12 meses");
+             int sumita = 0;
+
+            for (int mes = 0; mes <= 15; mes++)
+            {
+                Console.WriteLine("Poneme el valor del mes:" + mes);
+                string valor = Console.ReadLine();
+                sumita = sumita + int.Parse(valor);
+            }
+            Console.WriteLine(sumita);
+        }
+    }
+}
+
+
+
+
+
+
+
+/* Ejercicio para calcular la edad
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Puede ingresar su año de nacimiento?");
+string LoQueElTipoPuso = Console.ReadLine();
+// si voy a calcular mas o menos la edad que tiene tomando su año de nacimiento.
+// tendria que calcular así:
+int ElAnioDelPibeEnInt = int.Parse(LoQueElTipoPuso);
+int ElAnioDondeEstoyAhora = DateTime.Today.Year;
+int LaEdadDelFlaco = ElAnioDondeEstoyAhora - ElAnioDelPibeEnInt;
+
+Console.WriteLine("Vos tenes o vas a cumplir este año: " + LaEdadDelFlaco);
+*\
+
---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int opcion = 0;
+        do
+        {
+            Console.WriteLine("Seleccione una figura para calcular su volumen:");
+            Console.WriteLine("1. Cilindro");
+            Console.WriteLine("2. Esfera");
+            Console.WriteLine("3. Ortoedro");
+            Console.WriteLine("4. Prisma");
+            Console.WriteLine("5. Salir");
+            Console.Write("Opción: ");
+            opcion = int.Parse(Console.ReadLine());
+
+            double volumen = 0;
+            switch (opcion)
+            {
+                case 1:
+                    Console.Write("Ingrese el radio del cilindro: ");
+                    double radioCilindro = double.Parse(Console.ReadLine());
+                    Console.Write("Ingrese la altura del cilindro: ");
+                    double alturaCilindro = double.Parse(Console.ReadLine());
+                    volumen = Math.PI * radioCilindro * radioCilindro * alturaCilindro;
+                    Console.WriteLine("El volumen del cilindro es: " + volumen);
+                    break;
+                case 2:
+                    Console.Write("Ingrese el radio de la esfera: ");
+                    double radioEsfera = double.Parse(Console.ReadLine());
+                    volumen = (4.0 / 3.0) * Math.PI * Math.Pow(radioEsfera, 3);
+                    Console.WriteLine("El volumen de la esfera es: " + volumen);
+                    break;
+                case 3:
+                    Console.Write("Ingrese la longitud del ortoedro: ");
+                    double longitudOrtoedro = double.Parse(Console.ReadLine());
+                    Console.Write("Ingrese el ancho del ortoedro: ");
+                    double anchoOrtoedro = double.Parse(Console.ReadLine());
+                    Console.Write("Ingrese la altura del ortoedro: ");
+                    double alturaOrtoedro = double.Parse(Console.ReadLine());
+                    volumen = longitudOrtoedro * anchoOrtoedro * alturaOrtoedro;
+                    Console.WriteLine("El volumen del ortoedro es: " + volumen);
+                    break;
+                case 4:
+                    Console.Write("Ingrese la longitud de la base del prisma: ");
+                    double longitudPrisma = double.Parse(Console.ReadLine());
+                    Console.Write("Ingrese la altura del prisma: ");
+                    double alturaPrisma = double.Parse(Console.ReadLine());
+                    Console.Write("Ingrese el número de lados de la base del prisma: ");
+                    int numLadosPrisma = int.Parse(Console.ReadLine());
+                    double apotemaPrisma = (longitudPrisma / (2 * Math.Tan(Math.PI / numLadosPrisma)));
+                    volumen = (numLadosPrisma * Math.Pow(longitudPrisma, 2) * apotemaPrisma) / 4 * alturaPrisma;
+                    Console.WriteLine("El volumen del prisma es: " + volumen);
+                    break;
+                case 5:
+                    Console.WriteLine("¡Hasta luego!");
+                    break;
+                default:
+                    Console.WriteLine("Opción no válida. Intente de nuevo.");
+                    break;
+            }
+
+            Console.WriteLine();
+        } while (opcion != 5);
+    }
+}
